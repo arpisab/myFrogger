32207eceeab63da42d53747040e823e469d81900
arpisab <sabino.dargenio@gmail.com>
2014-11-08 09:34:18 +0000

    added myFrog.h

---
+++ myFrog.h
@@ -0,0 +1,38 @@
+////////////////////////////////////////////////////////////////////////////////
+/////
+///// Unknown SAB
+/////
+///// Class for the Frog Mesh
+/////
+
+#ifndef MYFROG_INCLUDED
+#define MYFROG_INCLUDED
+
+namespace octet {
+	//// Class for the main character of the game
+	class myFrog : public app {
+
+		////
+
+		
+			
+			
+		mesh *colladaMesh;  
+		////
+
+			
+
+	public:
+		myFrog() {
+		}
+
+		~myFrog() {
+		}
+		///
+			
+	};
+
+}
+
+#endif
+

602e45c2f672bfbc337bbbe282ab62bf3e963471
arpisab <sabino.dargenio@gmail.com>
2014-11-08 10:15:55 +0000

    added all

---
+++ allMeshes.h
@@ -0,0 +1,43 @@
+////////////////////////////////////////////////////////////////////////////////
+/////
+///// Unknown SAB
+/////
+///// All Meshes
+/////
+
+/*
+
+#ifndef ALLMESHES_INCLUDED
+#define ALLMESHES_INCLUDED
+
+namespace octet {
+  /// Class for all meshes
+	class allMeshes {
+		
+	protected:
+		mat4t modelToWorld;
+		material *mat;
+		scene_node *node;
+		btScalar mass;
+		btMotionState *motionState;
+		btRigidBody *rigidbody;
+		mesh *colladaMesh;  // only to be used when loading meshes from collada
+
+    public:
+      
+      allMeshes() {
+      }
+
+      
+	  ~allMeshes() {
+      }
+
+     
+
+   };
+  
+}
+
+#endif
+
+*/
